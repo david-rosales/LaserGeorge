cbf9d860bf9e3d7a34a5b23dac492e4a421c66bb
ChenkaiMao97 <chenkai@mit.edu>
2019-03-02 18:56:18 -0500

    ip needs to be changed

--- laser_george_communication/laserServer.py
+++ laser_george_communication/laserServer.py
@@ -2,14 +2,14 @@
 
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     while True:
-        s.listen()
+        s.listen(1)
         conn, addr = s.accept()
         with conn:
             print('Connected by', addr)
